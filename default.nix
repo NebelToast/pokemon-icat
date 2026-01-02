8ee987ccf72ff3a04b6772ddf8af7230dd97ae15
Julius <102417220+NebelToast@users.noreply.github.com>
2026-01-02 15:21:35 +0100

    Update outputHash in default.nix

--- default.nix
+++ default.nix
@@ -38,7 +38,7 @@ let
         # outputHash = "sha256-tJ7DIF1xzXHAyA2yEbmHJsKAZxhcgo4WjHLygGhpMHQ=";
         # outputHash = "sha256-65YMTrTk/XwGXcF3okj41zIllLZAZaMmD4g/4gO6C5A=";
         # outputHash = "sha256-1BVoArRKcryCayF31jGDKnFLjeBlU5HuxAzPnnQYAoA=";  # veihftm/JOfEiKa+ybT0aQD99J/eDPFCeyO43YU48Ds=
-        outputHash = "sha256-D1eb91IJJL+GbwA5bhL1m1iPGYb1Io99BNMfrkBJ4sU=";  # Commit 93844db8 without Alcremie forms
+        outputHash = "sha256-XEPbxKO5mDdUXdfsVEFSTMVscqrJC0Mf4zmfqUxrdJ4=";
         
         # src = ./.;
             src = pkgs.lib.fileset.toSource {
